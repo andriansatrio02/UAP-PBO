46f7e5355bae85edec3ba2bcdb8745fe5aa4fda2
andriansatrio02 <120787510+andriansatrio02@users.noreply.github.com>
2022-12-19 23:01:27 +0700

    Update penjualModel.java

--- Model/penjualModel.java
+++ Model/penjualModel.java
@@ -17,44 +17,44 @@ public class penjualModel {
                 + peju.getJumlahProduk() + "', '" + peju.getStok()+"','"+peju.getHarga()+"');"; 
         try {
            if( CONN.createStatement().executeUpdate(insert)>0){
-            System.out.println("Data Berhasil Dimasukkan");
+            System.out.println("Berhasil Dimasukkan");
            }else{
-               System.out.println("Data yang dimasukkan sudah ada");
+               System.out.println("Data sudah ada");
            }     
         } catch (SQLException ex) {
             Logger.getLogger(penjualModel.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Data Gagal Dimasukkan");
+            System.out.println("Gagal Dimasukkan");
         }
     }
     
-     public void updateProduk(Penjualan peju){//untuk memperbarui data pada query
+     public void updateProduk(Penjualan peju){
         String update = "UPDATE penjualan SET jumlahProduk = '"+ peju.getJumlahProduk() +"', stok = '"+ peju.getStok()+"' WHERE stok = '"+peju.getStok()+"';";
 
         try {
            if( CONN.createStatement().executeUpdate(update)>0){
-            System.out.println("Data Berhasil Diperbarui");
+            System.out.println("Berhasil Diperbarui");
            }else{
                System.out.println("Data yang dimasukkan tidak berubah");
            }     
         } catch (SQLException ex) {
             Logger.getLogger(penjualModel.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Data Gagal Dimasukkan");
+            System.out.println("Gagal Dimasukkan");
         }
      }
      
-     public void deleteProduk(Penjualan peju){//menghapus data didalam query
+     public void deleteProduk(Penjualan peju){
         String delete = "DELETE FROM penjualan WHERE nama_produk = '"+ peju.getNama_produk()+"';";
         
          try {
            if( CONN.createStatement().executeUpdate(delete)>0){
-            System.out.println("Data Berhasil Dihapus");
-//                  System.out.println(insert);
+            System.out.println("Berhasil Dihapus");
+
            }else{
-               System.out.println("Data Gagal dihapus");
+               System.out.println("Gagal dihapus");
            }     
         } catch (SQLException ex) {
             Logger.getLogger(penjualModel.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Data Gagal dihapus");
+            System.out.println("Gagal dihapus");
         }  
     }
 }
